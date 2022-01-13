e5410287da749d0c2594479fcf923ab8f2e4926b
Erik Ymer Jr <erik.ymer.jr@gmail.com>
2022-01-13 11:46:48 -0500

    Added in endpoints to search players and get a players segment stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var platform= '5';
-var platformUserIdentifier= 'AmbitiouslyErik';
+var platform= '';
+var platformUserIdentifier= '';
 
 fetch (`https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/apex/standard/profile/${platform}/${platformUserIdentifier}`,
 {
@@ -13,11 +13,31 @@ fetch (`https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/ape
     console.log(data)
 })
 
-// fetch(url).then(response => 
-//     response.json().then(data => ({
-//         data: data,
-//         status: response.status
-//     })
-// ).then(res => {
-//     console.log(res.status, res.data.title)
-// }));
+var platform= '';
+var gamertag= '';
+
+fetch (`https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/apex/standard/search`,
+{
+    headers: { 
+        'TRN-api-Key': '1a4488dd-ebfd-4fe6-b679-e353929b097e'
+    } 
+
+})
+.then(res => console.log(res.json()))
+.then(data =>{
+    console.log(data)
+})
+
+var segment= '';
+
+fetch (`https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/apex/standard/profile/${platform}/${platformUserIdentifier}/segments/${segmentType}`,
+{
+    headers: { 
+        'TRN-api-Key': '1a4488dd-ebfd-4fe6-b679-e353929b097e'
+    } 
+
+})
+.then(res => console.log(res.json()))
+.then(data =>{
+    console.log(data)
+})
